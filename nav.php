6ae7f956aba946609fdac2eab9550ba7c70d6164
darkmelcof <darkmelcof@gmail.com>
2016-08-07 18:38:22 +0200

    Test php
    Test page php

---
+++ nav.php
@@ -0,0 +1,11 @@
+<div class="mdl-layout__drawer">
+	<span class="mdl-layout-title">Clément</span>
+
+	<nav class="mdl-navigation">
+		<a class="mdl-navigation__link" href="index.html">Home</a>
+		<a class="mdl-navigation__link" href="projects.html">Projects</a>
+		<a class="mdl-navigation__link" href="about.html">About</a>
+		<a class="mdl-navigation__link" href="socials.html">Linkedin</a>
+	</nav>
+
+</div>
